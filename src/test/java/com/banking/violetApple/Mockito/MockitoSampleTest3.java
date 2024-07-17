4d08753903e93cc959e230ac7110c74cc73bac1c
siddharthsharma <siddharth54330@gmail.com>
2024-07-17 20:42:57 +0530

    MockitoExamples

---
+++ src/test/java/com/banking/violetApple/Mockito/MockitoSampleTest3.java
@@ -0,0 +1,27 @@
+package com.banking.violetApple.Mockito;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MockitoSampleTest3 {
+    // MockSingleElementTest
+    @SuppressWarnings("unchecked")
+    @Test
+    public void testMethod() {
+        ArrayList<String> mockedList = mock(ArrayList.class);
+
+        when(mockedList.get(0)).thenReturn("first-element");
+
+        System.out.println(mockedList.get(0));
+        assertEquals("first-element", mockedList.get(0));
+
+        // "null" gets printed as get(1) is not stubbed
+        System.out.println(mockedList.get(1));
+    }
+
+}
